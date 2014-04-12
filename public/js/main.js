5f3996ab25fa8c8fdf08cac0be113eb36cbdf660
James Hamet <jameshamet@gmail.com>
2014-04-12 19:25:16 -0400

    Automated commit message

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,24 @@ var alert = function(msg, title, buttonText){
         buttonText = "Got it!"   
     }
     intel.xdk.notification.alert(msg, title, buttonText)
-}
+};
+
+var user_latitude = position.coords.latitude;
+var user_longitude = position.coords.longitude;
+var getLocation = function(){
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+    user_latitude = position.coords.latitude;
+    user_longitude = position.coords.longitude;
+  }
+  else{
+      alert("Geolocation is disabled! Please enable it.");
+  }
+};
 
 var onDeviceReady=function(){
     intel.xdk.device.hideSplashScreen();
+    
 };
 
 document.addEventListener("intel.xdk.device.ready",onDeviceReady,false);
