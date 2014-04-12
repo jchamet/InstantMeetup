4ff09d19a9801bdd7ca8b9efeb4f1ce2f37a24af
James Hamet <jameshamet@gmail.com>
2014-04-12 19:58:51 -0400

    Automated commit message

--- public/js/pagenav.js
+++ public/js/pagenav.js
@@ -2,7 +2,7 @@ var onPageLoad = function(pageName) {
 	if (pageName == 'mypagehere') {
 		// Do something before the page loads/as it loads
 	} else if (pageName == 'home_page'){
-		initializeMap();   
+        google.maps.event.addDomListener(window, 'load', initializeMap); 
 	} else {
 		// Page not found
 	}
