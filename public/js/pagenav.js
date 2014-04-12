25e03f6d8c2e7b3cdbe116eeb81923e8b8c219fc
apourchet <antoine.pourchet@gmail.com>
2014-04-12 19:42:55 -0400

    Automated commit message

---
+++ public/js/pagenav.js
@@ -0,0 +1,10 @@
+var onPageLoad = function(pageName) {
+	if (pageName == 'mypagehere') {
+		// Do something before the page loads/as it loads
+	} else if (pageName == 'otherpagename'){
+		
+	} else {
+		// Page not found
+	}
+
+}
