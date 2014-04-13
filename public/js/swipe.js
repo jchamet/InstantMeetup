af5642ff370dd048b90b4e9b56f378f8bf3fdf80
James Hamet <jameshamet@gmail.com>
2014-04-13 02:18:24 -0400

    Automated commit message

---
+++ public/js/swipe.js
@@ -0,0 +1,13 @@
+$(document).on("swipeleft", 'div.ui-page', function(){
+    var nextpage = $(this).next('div[data-role="page"]');
+    if (nextpage.length > 0) {
+        $.mobile.changePage(nextpage, {transition:"slide"}, false, true);
+    }
+});
+$(document).on("swiperight", 'div.ui-page', function(){
+    var prevpage = $(this).prev('div[data-role="page"]');
+    if (prevpage.length > 0) {
+        $.mobile.changePage(prevpage, {transition: "slide",
+        reverse: true}, true, true);
+    }
+});
